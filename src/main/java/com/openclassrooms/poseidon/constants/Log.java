f139aebcb89154ac63453f17b57d9e5f4490943f
HashTucE <jeremyb59@wanadoo.fr>
2022-12-21 15:36:14 +0100

    add constants

---
+++ src/main/java/com/openclassrooms/poseidon/constants/Log.java
@@ -0,0 +1,17 @@
+package com.openclassrooms.poseidon.constants;
+
+public class Log {
+
+
+    //CONTROLLERS//
+
+    public static final String OBJECT_CREATED = "Object created successfully. Returning status 201 (Ok)";
+    public static final String OBJECT_MODIFIED = "Object updated successfully. Returning status 204 (Ok)";
+    public static final String OBJECT_DELETED = "Object deleted successfully. Returning status 204 (Ok)";
+    public static final String OBJECT_FOUND = "Object found successfully. Returning status 200 (Ok)";
+    public static final String OBJECT_NOT_FOUND = "Object not found. Returning status 404 (Not Found)";
+
+
+
+
+}
